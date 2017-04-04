670796aed4d210b601779ee8d00e92ca6a1225a3
Miloszex <milosz.weda@o2.pl>
2017-04-04 10:34:36 +0200

    dzialaja relacje

--- app/Sign.php
+++ app/Sign.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Sign extends Model
 {
-    //
+    protected $fillable = ['user_id', 'subject_id'];
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+    public function subject()
+    {
+        return $this->belongsTo('App\Subject');
+    }
 }
--- app/Subject.php
+++ app/Subject.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Subject extends Model
 {
-    //
+    protected $fillable = ['name', 'day' , 'begin_at','end_at', 'actual_space', 'space'];
+
+    public function users()
+    {
+        return $this->hasMany('App\User');
+    }
 }
